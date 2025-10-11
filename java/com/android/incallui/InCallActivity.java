065edcded720d9b55e1eda9e7cc64d2750b885fe
OothecaPickle <OothecaPickle@users.noreply.github.com>
2025-10-11 01:02:51 -0700

    .

--- java/com/android/incallui/InCallActivity.java
+++ java/com/android/incallui/InCallActivity.java
@@ -272,7 +272,11 @@ public class InCallActivity extends TransactionSafeFragmentActivity
 
   private void toggleSpeakerOnCallStart() {
     final int audioRoute = getAudioRoute();
-    if (audioRoute != CallAudioState.ROUTE_SPEAKER && audioRoute != CallAudioState.ROUTE_BLUETOOTH) {
+    //is checking that neither getSupportedRouteMask()
+    //nor getAudioRoute() contain ROUTE_BLUETOOTH redundant?
+    if (audioRoute != CallAudioState.ROUTE_SPEAKER && audioRoute != CallAudioState.ROUTE_BLUETOOTH
+        && 0 == (CallAudioState.ROUTE_BLUETOOTH
+        & AudioModeProvider.getInstance().getAudioState().getSupportedRouteMask())) {
       TelecomAdapter.getInstance().setAudioRoute(CallAudioState.ROUTE_SPEAKER);
       TelecomAdapter.getInstance().setAudioRoute(audioRoute);
     }
